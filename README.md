daa9acad876559a0a9aa2bf9209274c6258575c5
kuronoraida <99105705+kuronoraida@users.noreply.github.com>
2023-11-29 15:40:25 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webpack-template
+Template for webpack environment. Includes npm, webpack, webpack-cli, html-webpack-plugin, style-loader, css-loader.
+Run npx webpack --watch to build and start watching for changes.
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
