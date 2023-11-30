14089dbe715593d89a6596dec12d6122cb4a3ce0
kuronoraida <99105705+kuronoraida@users.noreply.github.com>
2023-11-30 11:45:42 +1100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
